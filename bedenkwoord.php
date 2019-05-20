a52a25897b23549df3d1f9a1160d1479ca3f3b27
Jacco Gritter <jacco.gritter@gmail.com>
2019-05-20 20:57:08 +0200

    more words added

--- bedenkwoord.php
+++ bedenkwoord.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-$woord = array ("boten", "balen", "beter", "boren", "broek", "brand", "beeld", "brief", "bloed", "broer", "blond", "boter", "beleg", "bazin", "bezem", "baard", "bloem", "maand", "media", "meter", "motor", "markt", "molen"
+$woord = array ("boten", "balen", "beter", "boren", "broek", "brand", "beeld", "brief", "bloed", "broer", "blond", "boter", "beleg", "bazin", "bezem", "baard", "bloem", "maand", "media", "meter", "motor", "markt", "molen", "fiets", "friet", "forel", "feest", "fruit", "fauna", "freak", "forum", "fusie", "steek", "schep", "spijs", "stoep", "shirt", "sport", "spalk", "sjaal", "storm", "staat", "steun", "serie", "schat", "snoep", "sfeer", "speer", "scene", "speld", "smaak", "super", "stand", "skier", "sluis", "steel", "snaar", "spuit", "tegel", "taart", "tafel", "trouw", "teken", "taken", "tenen", "titel", "woord", "wagen", "wonen", "waren", "zadel", "zweet", "zenuw", "zweer"
 );
  
 $n = rand(0, count($woord));    // pick a random number
