134865b1e87d8c8001d8efa6b074ccf0f7c91334
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-02 15:09:22 +0200

    word list loaded from file now

--- bedenkwoord.php
+++ bedenkwoord.php
@@ -2,12 +2,15 @@
 
 session_start();
 
-$woord = array ("boten", "balen", "beter", "boren", "broek", "brand", "beeld", "brief", "bloed", "broer", "blond", "boter", "beleg", "bazin", "bezem", "baard", "bloem", "maand", "media", "meter", "motor", "markt", "molen", "fiets", "friet", "forel", "feest", "fruit", "fauna", "freak", "forum", "fusie", "steek", "schep", "spijs", "stoep", "shirt", "sport", "spalk", "sjaal", "storm", "staat", "steun", "serie", "schat", "snoep", "sfeer", "speer", "scene", "speld", "smaak", "super", "stand", "skier", "sluis", "steel", "snaar", "spuit", "tegel", "taart", "tafel", "trouw", "teken", "taken", "tenen", "titel", "woord", "wagen", "wonen", "waren", "zadel", "zweet", "zenuw", "zweer"
-);
- 
-$n = rand(0, count($woord));    // pick a random number
+$myfile = fopen("woordenlijst5.txt", "r") or die("Unable to open file!");
+$woordenlijst = fread($myfile, filesize("woordenlijst5.txt"));
+fclose($myfile);
 
-$_SESSION["teradenwoord"] = $woord[$n];
+$woordenArray = explode("\n", $woordenlijst); 
+
+$n = rand(0, count($woordenArray));    // pick a random number
+
+$_SESSION["teradenwoord"] = $woordenArray[$n];
 $_SESSION["beurt"] = 0;
 
 ?>
